3d85a58cf9ef88caa872e2fd3ef40e46fdeea895
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-08-29 06:18:40 +0200

    v1.0.3 : fix minui_bm Android.mk
    change usb tools labels and update scripts
    
    Bootmenu full version now require BUILD_BOOTMENU_STANDALONE
    
    Change-Id: Ia92071251409a5ac299b4daf6c3bc4e2b6d3e38e

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ ifneq ($(TARGET_SIMULATOR),true)
 ################################
 
 LOCAL_PATH := $(call my-dir)
-commands_bootmenu_local_path := $(LOCAL_PATH)
+bootmenu_local_path := $(LOCAL_PATH)
 
 bootmenu_sources := \
     extendedcommands.c \
@@ -13,11 +13,11 @@ bootmenu_sources := \
     default_bootmenu_ui.c \
     ui.c \
 
-include $(CLEAR_VARS)
-
-BOOTMENU_VERSION:=1.0.2
+BOOTMENU_VERSION:=1.0.3
 
 ifeq ($(BOARD_USES_BOOTMENU),true)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := bootmenu
 LOCAL_MODULE_TAGS := eng
@@ -27,6 +27,8 @@ LOCAL_SRC_FILES := $(bootmenu_sources)
 BOOTMENU_SUFFIX :=
 
 LOCAL_CFLAGS += -DBOOTMENU_VERSION="${BOOTMENU_VERSION}${BOOTMENU_SUFFIX}" -DFULL_VERSION=0
+
+PRODUCT_PACKAGES += libminui_bm
 
 LOCAL_STATIC_LIBRARIES :=
 LOCAL_STATIC_LIBRARIES += libminui_bm libpixelflinger_static libpng libz
@@ -38,20 +40,21 @@ LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/bin
 
 include $(BUILD_EXECUTABLE)
 
-include $(call all-makefiles-under,$(commands_bootmenu_local_path))
-
-include $(CLEAR_VARS)
+include $(call all-makefiles-under,$(bootmenu_local_path))
 
 endif # BOARD_USES_BOOTMENU
 
 
 ############################
 # Standalone version
+ifeq ($(BUILD_BOOTMENU_STANDALONE),1)
 
-LOCAL_PATH := $(commands_bootmenu_local_path)
+LOCAL_PATH := $(bootmenu_local_path)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := Bootmenu
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 
 LOCAL_SRC_FILES := $(bootmenu_sources)
 
@@ -66,11 +69,13 @@ LOCAL_STATIC_LIBRARIES += libstdc++ libc libcutils
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/bootmenu/binary
-
-include $(call all-makefiles-under,$(commands_bootmenu_local_path)/minui)
+LOCAL_MODULE_STEM := bootmenu-standalone
 
 include $(BUILD_EXECUTABLE)
 
+include $(call all-makefiles-under,$(bootmenu_local_path))
+
+endif
 ###########################
 
 endif # !TARGET_SIMULATOR
